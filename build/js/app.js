3d13f1c1eff7e62a1e0274bef3ae5886c7cd0c1f
Guillermo <memo.9321@gmail.com>
2016-08-17 18:30:59 -0500

    Espacios

---
+++ build/js/app.js
@@ -0,0 +1,22 @@
+(function(){
+	var app = angular.module('loveitSite', [
+		'ngRoute',
+		'loveitSite.controllers',
+		'loveitSite.directives'
+	]);
+	app.config(['$routeProvider', '$locationProvider' , function($routeProvider, $locationProvider){
+		$routeProvider
+			.when('/home', {
+				templateUrl: './views/home.html',
+				controller: 'menuItemController'
+			})
+			.when('/espacios', {
+				templateUrl: './views/espacios.html',
+				controller: 'menuItemController'
+			})
+			.otherwise({
+				redirectTo: '/home'
+			});
+			// $locationProvider.html5Mode(true);
+	}]);
+})();
