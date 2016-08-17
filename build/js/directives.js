35112b8c3b515dbbebaf7c16fce59e056b692769
Guillermo <memo.9321@gmail.com>
2016-08-17 15:38:30 -0500

    Home.html

--- build/js/directives.js
+++ build/js/directives.js
@@ -5,7 +5,47 @@
       restrict: 'E',
       templateUrl: './partials/header-top.html',
       controller: function($document){
+        var status = false;
+        var tam = 0;
+        function verifySize(){
+          tam = $(window).width();
+          if(tam<767){
+            $('.headerNav').slideUp();
+            status = false;
+          }else{
+            $('.headerNav').slideDown();
+          }
+        }
+        $(window).resize(function(){
+          verifySize();
+        });
+        verifySize();
+        $(document).on('click', '.menu label#hamburger input', function(){
+          if(status){
+            $('.headerNav').slideUp();
+          }else{
+            $('.headerNav').slideDown();
+          }
+          status = !status;
+        });
       }
     }
+  })
+  .directive('swiperSlider', function(){
+    return {
+      restrict: 'E',
+      templateUrl: './partials/swiper-slider.html',
+      controller: function($document){
+        setTimeout(function(){
+          var mySwiper = new Swiper('.swiper-container',{
+            pagination: '.swiper-pagination',
+            loop:true,
+            grabCursor: false,
+            paginationClickable: true,
+            autoplay:false
+          });
+        },80);
+      }
+    };
   })
 })();
